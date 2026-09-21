2cf3a34acd488b21257c95801a2072e9790d7cbd
THANHTRUONG06082006-VN <thanh.2474802010361@vanlanguni.vn>
2026-09-21 15:09:36 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ function login(username, password) {
   if (username === 'locked_user') {
     return false;
   }
-  if (username === 'admin' && password === '9999') {
+  if (username === 'admin' && password === '123') {
     return true;
   }
   return false;
